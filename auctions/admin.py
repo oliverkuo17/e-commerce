c80a989723462fb1fb3d21493ee8a8562c524942
oliverkuo17 <oliverkuo17@gmail.com>
2021-02-06 16:30:22 +1000

    updating models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import User, Listing, Comment, Bid, Watchlist
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Comment)
+admin.site.register(Bid)
+admin.site.register(Watchlist)
--- auctions/models.py
+++ auctions/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -18,6 +20,26 @@ class Listing(models.Model):
         ("FnG", "Food & Grocery"),
         ("OTH", "Others")
     )
+
+    consignor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     item_name = models.CharField(max_length=64)
-    category = models.CharField(max_length=32, choices=CATEGORY_CHOICES, default = 'OTH')
+    description = models.TextField(max_length=2048)
+    category = models.CharField(max_length=32, choices=CATEGORY_CHOICES, default='OTH')
+    photo = models.ImageField(upload_to='%Y/%m/%D/')
     deadline = models.DateTimeField()
+    current_price = models.DecimalField(max_digits=9, decimal_places=2, default=1.00)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    title = models.CharField(max_length=64)
+    contenct = models.CharField(max_length=1024)
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    amount = models.DecimalField(max_digits=9, decimal_places=2, validators=[MinValueValidator(('0.01'))])
+
+class Watchlist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="watchlist")
+    items = models.ManyToManyField(Listing, blank=True, related_name="watchlist")
