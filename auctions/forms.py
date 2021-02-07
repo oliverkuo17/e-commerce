f951cae542eaf7746a70de5840233d8ca1677e72
oliverkuo17 <oliverkuo17@gmail.com>
2021-02-07 22:38:01 +1000

    update form styling

--- auctions/forms.py
+++ auctions/forms.py
@@ -6,6 +6,15 @@ class CreateListingForm(ModelForm):
     class Meta:
         model = Listing
         fields = ['item_name', 'description', 'category', 'photo', 'photo_name', 'deadline','starting_bid']
+        widgets = {
+            'item_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'photo': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'photo_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'deadline': forms.DateTimeInput(attrs={'class': 'form-control'}),
+            'starting_bid': forms.NumberInput(attrs={'class': 'form-control'})
+        }
 
 class CreateCommentForm(ModelForm):
     class Meta:
