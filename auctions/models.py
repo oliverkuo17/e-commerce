dbd30dd3cefee36b52d582a6791c4ac909cf19a1
oliverkuo17 <oliverkuo17@gmail.com>
2021-02-08 10:23:03 +1000

    update db

--- auctions/models.py
+++ auctions/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return f'user_{instance.user.id}/{filename}'
+    return f'user_{instance.seller.id}/{filename}'
 
 class User(AbstractUser):
     pass
